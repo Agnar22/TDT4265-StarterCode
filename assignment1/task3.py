8cbdfdcf57b16e9d06b00dfbc2723bde583af1f7
Agnar22 <agnar.bjornstad@gmail.com>
2021-01-24 13:46:23 +0100

    Save as one image.

--- assignment1/task3.py
+++ assignment1/task3.py
@@ -129,11 +129,16 @@ if __name__ == "__main__":
     # You can finish the rest of task 4 below this point.
 
     # Plotting of softmax weights (Task 4b)
+    image = None
     for img in range(10):
         weight = model.w[:-1,img]
         weight = weight - np.min(weight)
         weight = weight / np.max(weight)
-        plt.imsave(f'task4b_softmax_weight_{img}.png', weight.reshape((28, 28)), cmap="gray")
+        if image is None:
+            image = weight.reshape((28, 28))
+        else:
+            image = np.hstack((image, weight.reshape((28, 28))))
+    plt.imsave(f'task4b_softmax_weight.png', image, cmap="gray")
 
     # Plotting of accuracy for difference values of lambdas (task 4c)
     l2_lambdas = [1, .1, .01, .001]
