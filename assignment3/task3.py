c23fa513fec7e6b5fb4a67bbd98d371f53f374d9
Agnar22 <agnar.bjornstad@gmail.com>
2021-03-02 11:29:02 +0100

    Create nn more dynamically.

--- assignment3/task3.py
+++ assignment3/task3.py
@@ -6,34 +6,45 @@ import task2
 import torch
 import matplotlib.pyplot as plt
 from trainer import Trainer
+from typing import List
 
 
-def create_conv(in_channels, num_filters, batch_norm, batch_norm_affine):
+def create_conv(in_channels, num_filters, batch_norm, batch_norm_affine, kernel_size):
   return [
-    nn.Conv2d(in_channels, num_filters, kernel_size=3, stride=1, padding=2),
+    nn.Conv2d(in_channels, num_filters, kernel_size=kernel_size, stride=1, padding=2),
     nn.ReLU(inplace=True),
     nn.BatchNorm2d(num_filters, affine=batch_norm_affine),
-    nn.Conv2d(num_filters, num_filters, kernel_size=3, stride=1, padding=2),
+    nn.Conv2d(num_filters, num_filters, kernel_size=kernel_size, stride=1, padding=2),
     nn.ReLU(inplace=True),
     nn.MaxPool2d(2, stride=2),
     nn.BatchNorm2d(num_filters, affine=batch_norm_affine)
   ] if batch_norm else [
-    nn.Conv2d(in_channels, num_filters, kernel_size=3, stride=1, padding=2),
+    nn.Conv2d(in_channels, num_filters, kernel_size=kernel_size, stride=1, padding=2),
     nn.ReLU(inplace=True),
-    nn.Conv2d(num_filters, num_filters, kernel_size=3, stride=1, padding=2),
+    nn.Conv2d(num_filters, num_filters, kernel_size=kernel_size, stride=1, padding=2),
     nn.ReLU(inplace=True),
     nn.MaxPool2d(2, stride=2),
   ]
 
+def create_linear(in_shape, nodes, dropout):
+  return [
+    nn.Linear(in_shape, nodes),
+    nn.ReLU(inplace=True),
+    nn.Dropout(dropout),
+  ]
 
 class ExampleModel(nn.Module):
 
   def __init__(self,
                image_channels,
                num_classes,
+               conv_layers: List[int],
+               num_output_features: int,
+               dense_layers: List[int],
                batch_norm: bool = False,
                batch_norm_affine: bool = False,
-               dropout: float = 0):
+               dropout: float = 0,
+               kernel_size=3):
     """
         Is called when model is initialized.
         Args:
@@ -41,38 +52,41 @@ class ExampleModel(nn.Module):
             num_classes: Number of classes we want to predict (10)
     """
     super().__init__()
-    num_filters = 32  # Set number of filters in first conv layer
+    feature_extractor_layers = [image_channels] + conv_layers
     self.num_classes = num_classes
     # Define the convolutional layers
     self.feature_extractor = nn.Sequential(
-      *create_conv(image_channels, num_filters, batch_norm, batch_norm_affine),
-      *create_conv(num_filters, 2 * num_filters, batch_norm, batch_norm_affine),
-      *create_conv(2 * num_filters, 4 * num_filters, batch_norm, batch_norm_affine),
+      *[
+        sub_layer for layer in [
+          create_conv(
+            feature_extractor_layers[num_layer - 1],
+            feature_extractor_layers[num_layer],
+            batch_norm,
+            batch_norm_affine,
+            kernel_size
+          ) for num_layer in range(1, len(feature_extractor_layers))]
+        for sub_layer in layer
+      ]
     )
     self.feature_extractor.apply(self.init_weights)
     # The output of feature_extractor will be [batch_size, num_filters, 16, 16]
-    self.num_output_features = 128 * 7 * 7
+    self.num_output_features = num_output_features
+    linear_layers = [num_output_features] + dense_layers
     # Initialize our last fully connected layer
     # Inputs all extracted features from the convolutional layers
     # Outputs num_classes predictions, 1 for each class.
     # There is no need for softmax activation function, as this is
     # included with nn.CrossEntropyLoss
     self.classifier = nn.Sequential(
-      nn.Linear(self.num_output_features, 256),
-      nn.ReLU(inplace=True),
-      nn.Dropout(dropout),
-      # nn.BatchNorm1d(2048, affine=False),
-      nn.Linear(256, 128),
-      nn.ReLU(inplace=True),
-      nn.Dropout(dropout),
-      nn.Linear(128, 128),
-      nn.ReLU(inplace=True),
-      nn.Dropout(dropout),
-      # nn.BatchNorm1d(512, affine=False),
-      nn.Linear(128, 64),
-      nn.ReLU(inplace=True),
-      nn.Dropout(dropout),
-      # nn.BatchNorm1d(64, affine=False),
+      *[
+        sub_layer for layer in [
+          create_linear(
+            linear_layers[num_layer - 1],
+            linear_layers[num_layer],
+            dropout
+          ) for num_layer in range(1, len(linear_layers))
+        ] for sub_layer in layer
+      ],
       nn.Linear(64, num_classes),
     )
     self.classifier.apply(self.init_weights)
@@ -98,7 +112,7 @@ class ExampleModel(nn.Module):
       torch.nn.init.xavier_normal_(m.weight)
 
 
-def create_combined_plots(trainers: Dict[str,Trainer], name: str):
+def create_combined_plots(trainers: Dict[str, Trainer], name: str):
   plot_path = pathlib.Path("plots")
   plot_path.mkdir(exist_ok=True)
   # Save plots and show them
@@ -133,7 +147,17 @@ if __name__ == "__main__":
   # Best model.
   learning_rate = 1e-3
   early_stop_count = 4
-  best_model = ExampleModel(image_channels=3, num_classes=10, batch_norm=True, batch_norm_affine=False, dropout=0.2)
+  best_model = ExampleModel(
+    image_channels=3,
+    num_classes=10,
+    conv_layers=[32, 64, 128],
+    num_output_features=128*7*7,
+    dense_layers=[256, 128, 128, 64],
+    batch_norm=True,
+    batch_norm_affine=False,
+    dropout=0.2,
+    kernel_size=3
+  )
   best_trainer = task2.Trainer(batch_size, learning_rate, early_stop_count, epochs, best_model, dataloaders, adam=True)
   best_trainer.train()
   best_trainer.load_best_model()
@@ -149,8 +173,17 @@ if __name__ == "__main__":
   # Task 3 d)
   learning_rate = 1e-3
   early_stop_count = 4
-  best_model_no_batch = ExampleModel(image_channels=3, num_classes=10, batch_norm=False, batch_norm_affine=False,
-                                     dropout=0.2)
+  best_model_no_batch = ExampleModel(
+    image_channels=3,
+    num_classes=10,
+    conv_layers=[32, 64, 128],
+    num_output_features=128*7*7,
+    dense_layers=[256, 128, 128, 64],
+    batch_norm=False,
+    batch_norm_affine=False,
+    dropout=0.2,
+    kernel_size=3
+  )
   best_trainer_no_batch = task2.Trainer(batch_size, learning_rate, early_stop_count, epochs, best_model_no_batch,
                                         dataloaders, adam=True)
   best_trainer_no_batch.train()
@@ -160,7 +193,17 @@ if __name__ == "__main__":
   # Task 3 e)
   learning_rate = 5e-4
   early_stop_count = 4
-  improved_best_model = ExampleModel(image_channels=3, num_classes=10, batch_norm=True, batch_norm_affine=True, dropout=0.2)
+  improved_best_model = ExampleModel(
+    image_channels=3,
+    num_classes=10,
+    conv_layers=[32, 64, 128],
+    num_output_features=128*7*7,
+    dense_layers=[256, 128, 128, 64],
+    batch_norm=True,
+    batch_norm_affine=True,
+    dropout=0.2,
+    kernel_size=3
+  )
   improved_best_trainer = task2.Trainer(batch_size, learning_rate, early_stop_count, epochs, improved_best_model, dataloaders, adam=True)
   improved_best_trainer.train()
   improved_best_trainer.load_best_model()
